1ce78c2381714b0bec92f35faee73cb97e71d07b
tbaca <tbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-01-28 18:27:39 +0000

    Make score.

--- 2005/make-score.sh
+++ 2005/make-score.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env sh
 
-pdftk poeme-2005-fc.pdf poeme-2005-ifc.pdf poeme-2005-first-page.pdf poeme-2005-scaled.pdf blank_11_x_17_portrait.pdf poeme-2005-bc.pdf cat output score.pdf
+pdftk A=poeme-2005-fc.pdf B=poeme-2005-ifc.pdf C=poeme-2005-first-page.pdf D=poeme-2005-scaled.pdf E=blank_11_x_17_portrait.pdf F=poeme-2005-bc.pdf cat A B C D2-end E F output score.pdf
