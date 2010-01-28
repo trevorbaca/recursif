fb88c42909fffb6925765fe79c41cfbed8d13475
tbaca <tbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-01-28 18:34:50 +0000

    Make score.

--- 2005/make-score.sh
+++ 2005/make-score.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env sh
 
-pdftk A=poeme-2005-fc.pdf B=poeme-2005-ifc.pdf C=poeme-2005-first-page.pdf D=poeme-2005-scaled.pdf E=blank_11_x_17_portrait.pdf F=poeme-2005-bc.pdf cat A B C D2-end E F output score.pdf
+pdftk A=poeme-2005-fc.pdf B=poeme-2005-ifc.pdf C=poeme-2005-first-page.pdf D=poeme-2005-scaled.pdf E=poeme-2005-bc.pdf cat A B C D2-end E output score.pdf
+
+## note that the blank 11 x 17 portrait page is not necessary
