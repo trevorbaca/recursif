9c238be179d481aedc9e9f86eba56add6b0871c0
tbaca <tbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-01-29 19:38:41 +0000

    2005 parts.

--- 2005/parts/py/render_and_pdftk_parts.py
+++ 2005/parts/py/render_and_pdftk_parts.py
@@ -4,8 +4,8 @@ import os
 def make_score_ly_files( ):
    print 'making score ly files ...'
    for part_number in range(1, 65):
-      score_template = file('score-template.ly', 'r')
-      score_ly_file = file('part-%s-score.ly' % str(part_number).zfill(2), 'w')
+      score_template = file('ly/score-template.ly', 'r')
+      score_ly_file = file('ly/parts/%s.ly' % str(part_number).zfill(2), 'w')
       for line in score_template.readlines( ):
          if 'part-99' in line:
             line = line.replace('99', str(part_number).zfill(2))
@@ -19,16 +19,19 @@ def make_score_ly_files( ):
 def render_score_pdfs( ):
    print 'rendering pdfs ...'
    for part_number in range(1, 65):
-      command = 'lily part-%s-score.ly' % str(part_number).zfill(2)
+      part_number_string = str(part_number).zfill(2)
+      command = 'lily ly/parts/%s.ly' % part_number_string
       os.system(command)
-   os.system('rm *.ps')
+      command = 'mv ly/parts/%s.pdf pdf/' % part_number_string
+      os.system(command)
+   os.system('rm ly/parts/*.ps')
 
 
 def glom_score_pdfs( ):
    print 'glomming pdfs ...'
    pdftk_command = 'pdftk %s cat output poeme-2005-parts.pdf'
    pdf_file_names = [ ]
-   for file_name in os.listdir('.'):
+   for file_name in os.listdir('pdf/'):
       if file_name.startswith('part-') and file_name.endswith('.pdf'):
          pdf_file_names.append(file_name)
    pdf_file_names = ' '.join([str(x) for x in sorted(pdf_file_names)])
