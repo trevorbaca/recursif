384d08db6a56bed34c1c6058968bc82b10eccec4
tbaca <tbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-01-30 03:46:58 +0000

    2003 parts.

--- 2003/parts/py/render_pdfs.py
+++ 2003/parts/py/render_pdfs.py
@@ -3,7 +3,7 @@ import os
 
 def render_pdfs(score_directory):
    file_names = os.listdir(score_directory + '/ly')
-   for file_name in file_names[:3]:
+   for file_name in file_names:
       if file_name.endswith('.ly'):
          number = file_name[:-3]
          command = 'lilypond -o %s/pdf/%s %s/ly/%s' 
