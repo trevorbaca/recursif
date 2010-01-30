2c55b8f197744284b159c3ecb9cdae056573fc07
tbaca <tbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-01-30 03:17:59 +0000

    2003 parts.

---
+++ 2003/parts/py/render_pdfs.py
@@ -0,0 +1,16 @@
+import os
+
+
+def render_pdfs(score_directory):
+   file_names = os.listdir(score_directory + '/ly')
+   for file_name in file_names[:3]:
+      if file_name.endswith('.ly'):
+         number = file_name[:-3]
+         command = 'lilypond -o %s/pdf/%s %s/ly/%s' 
+         command %= (score_directory, number, score_directory, file_name)
+         os.system(command)
+
+
+if __name__ == '__main__':
+   score_directory = '/Users/trevorbaca/Documents/scores/recursif/2003/parts'
+   render_pdfs(score_directory)
