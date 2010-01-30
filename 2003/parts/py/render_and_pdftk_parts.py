7b6065a1452e7036c96c1748459e0f4b95f308ef
tbaca <tbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-01-30 02:40:03 +0000

    2003 parts.

---
+++ 2003/parts/py/render_and_pdftk_parts.py
@@ -0,0 +1,18 @@
+import os
+
+def render_pdfs( ):
+   pass
+
+
+def move_pdfs( ):
+   pass
+
+
+def pdftk_pdfs( ):
+   pass
+
+
+if __name__ == '__main__':
+   render_pdfs( )
+   move_pdfs( )
+   pdftk_pdfs( )
