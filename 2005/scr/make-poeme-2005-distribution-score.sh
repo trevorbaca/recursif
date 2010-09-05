5b804a5745bdf6df6208dfa936307ad70af45490
trevorbaca <trevorbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2010-09-05 22:04:56 +0000

    Score build improvements.

---
+++ 2005/scr/make-poeme-2005-distribution-score.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+
+FILENAME=poeme-2003-distribution-score
+INPUTDIR=~/Documents/scores/recursif/2003/score/tex
+OUTPUTDIR=~/Documents/scores/recursif/2003/_distribution
+
+pdflatex --jobname=${FILENAME} \
+   -output-directory=${OUTPUTDIR} \
+   ${INPUTDIR}/glue-${FILENAME}.tex
+
+clear
+rmlx ${OUTPUTDIR}
+
+echo "Distribution score complete.\n"
+echo "Directory: ${OUTPUTDIR}"
+echo "File:      ${FILENAME}.pdf\n"
