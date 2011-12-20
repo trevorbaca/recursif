dd5e30658f7cbe99f60496e69dcb3acf7d6337c4
trevorbaca <trevorbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2011-12-20 20:17:59 +0000

    Updated initializer.

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 
 tags = OrderedDict([
     ('composer', TrevorBaca()),
+    ('forces_tagline', 'for 64 pieces of percussion'),
     ('instrumentation', scoretools.InstrumentationSpecifier([])),
     ('title', 'Po\xc3\xa8me r\xc3\xa9cursif'),
     ('year_of_completion', 2005)])
