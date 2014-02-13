38c49cfafa096746d60ea730edb3ddeb67fc6174
trevorbaca <trevorbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2014-02-13 18:36:38 +0000

    Changed 'tags' to 'metadata' everywhere.

--- __metadata__.py
+++ __metadata__.py
@@ -2,7 +2,7 @@
 import collections
 
 
-tags = collections.OrderedDict([
+metadata = collections.OrderedDict([
     ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for 64 pieces of percussion'),
     ('title', 'Po\xc3\xa8me r\xc3\xa9cursif'),
