ac888f59a5cbd0ce952d465e5cbce2f118cadd8b
trevorbaca <trevorbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2014-03-19 16:19:34 +0000

    Rewrote metadata modules.

--- __metadata__.py
+++ __metadata__.py
@@ -6,4 +6,5 @@ metadata = collections.OrderedDict([
     ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for 64 pieces of percussion'),
     ('title', 'Po\xc3\xa8me r\xc3\xa9cursif'),
-    ('year_of_completion', 2005)])
+    ('year_of_completion', 2005),
+    ])
