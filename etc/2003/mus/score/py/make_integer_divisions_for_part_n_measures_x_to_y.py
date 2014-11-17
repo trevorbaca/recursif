9f46352080d5b185570a1861651e2995a3ea7444
trevorbaca <trevorbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2014-11-17 16:23:13 +0000

    Cleaned up helper function.

--- etc/2003/mus/score/py/make_integer_divisions_for_part_n_measures_x_to_y.py
+++ etc/2003/mus/score/py/make_integer_divisions_for_part_n_measures_x_to_y.py
@@ -1,11 +1,28 @@
-from abjad.tools import mathtools
+from abjad import *
 
 
 def make_integer_divisions_for_part_n_measures_x_to_y(n, x, y):
     '''Make integer divisions for part `n` 0-indexed measures `x` to `y`.
+
+    ::
+
+        >>> make_integer_divisions_for_part_n_measures_x_to_y(2, 1, 16)
+        [0, 7, 6, 5, 4, 3, 2, 1, 0, 7, 6, 5, 4, 3, 2, 1]
+
+    ::
+
+        >>> make_integer_divisions_for_part_n_measures_x_to_y(14, 177, 177+15)
+        [0, 4, 0, 4, 0, 2, 4, 2, 0, 4, 0, 4, 0, 1, 6, 1]
+
+    ::
+
+        >>> make_integer_divisions_for_part_n_measures_x_to_y(64, 241, 241+15)
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+
     '''
 
-    result = []
-    for i in range(x, y + 1):
-        result.append(int(mathtools.binomial_coefficient(255 + n - i, n - 1) % 8))
-    return result
+    divisions = []
+    for i in range(x, y+1):
+        division = int(mathtools.binomial_coefficient(255 + n-i, n-1) % 8)
+        divisions.append(division)
+    return divisions
