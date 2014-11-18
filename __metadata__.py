116af532959f1314283d822897c0e33d9068fae4
trevorbaca <trevorbaca@0737405c-5efb-42fd-9960-420b23c2da47>
2014-11-18 19:58:50 +0000

    Built draft.

--- __metadata__.py
+++ __metadata__.py
@@ -3,8 +3,11 @@ import collections
 
 
 metadata = collections.OrderedDict([
+    ('catalog_number', 'AWN-001'),
     ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for 64 pieces of percussion'),
+    ('paper_dimensions', '11 x 17 in'),
+    ('price', '\\euro 88 \\/ \\$114'),
     ('title', 'Po\xc3\xa8me r\xc3\xa9cursif'),
-    ('year', 2005),
+    ('year', 2003),
     ])
