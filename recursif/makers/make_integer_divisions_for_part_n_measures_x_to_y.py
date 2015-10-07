3ee9585537b0c2d2303b20f86024f3dc55aa240d
Trevor Bača <trevorbaca@gmail.com>
2015-10-07 10:50:10 -0400

    Moved helper function into makers directory.

--- recursif/makers/make_integer_divisions_for_part_n_measures_x_to_y.py
+++ recursif/makers/make_integer_divisions_for_part_n_measures_x_to_y.py
@@ -1,22 +1,25 @@
+# -*- coding: utf-8 -*-
 from abjad import *
 
 
 def make_integer_divisions_for_part_n_measures_x_to_y(n, x, y):
     '''Make integer divisions for part `n` 0-indexed measures `x` to `y`.
 
+        >>> import recursif
+
     ::
 
-        >>> make_integer_divisions_for_part_n_measures_x_to_y(2, 1, 16)
+        >>> recursif.makers.make_integer_divisions_for_part_n_measures_x_to_y(2, 1, 16)
         [0, 7, 6, 5, 4, 3, 2, 1, 0, 7, 6, 5, 4, 3, 2, 1]
 
     ::
 
-        >>> make_integer_divisions_for_part_n_measures_x_to_y(14, 177, 177+15)
+        >>> recursif.makers.make_integer_divisions_for_part_n_measures_x_to_y(14, 177, 177+15)
         [0, 4, 0, 4, 0, 2, 4, 2, 0, 4, 0, 4, 0, 1, 6, 1]
 
     ::
 
-        >>> make_integer_divisions_for_part_n_measures_x_to_y(64, 241, 241+15)
+        >>> recursif.makers.make_integer_divisions_for_part_n_measures_x_to_y(64, 241, 241+15)
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 
     '''
