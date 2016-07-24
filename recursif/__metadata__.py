2ec094dbc6207195a2c22f81c4e1f1cc6567600a
Trevor Bača <trevor.baca@gmail.com>
2016-07-24 17:09:40 -0500

    Updated metadata.

--- recursif/__metadata__.py
+++ recursif/__metadata__.py
@@ -6,7 +6,7 @@ metadata = collections.OrderedDict([
     ('catalog_number', 'AWN-001'),
     ('composer', 'Trevor Ba\xc4\x8da'),
     ('forces_tagline', 'for 64 pieces of percussion'),
-    ('paper_dimensions', '11 x 17 in'),
+    ('paper_size', '11 x 17 in'),
     ('price', '\\euro 88 \\/ \\$114'),
     ('title', 'Po\xc3\xa8me r\xc3\xa9cursif'),
     ('year', 2003),
