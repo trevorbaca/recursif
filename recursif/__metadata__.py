a2681fd3a5b053c6c75b43544e6df6ad4b0fe8df
Trevor Bača <trevor.baca@gmail.com>
2016-12-10 14:38:31 -0600

    Cleaned up import statements.

--- recursif/__metadata__.py
+++ recursif/__metadata__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-import collections
+import abjad
 
 
-metadata = collections.OrderedDict([
+metadata = abjad.datastructuretools.TypedOrderedDict([
     ('catalog_number', 'AWN-000-PR'),
     ('composer', 'Trevor Bača'),
     ('forces_tagline', 'for 64 pieces of percussion'),
