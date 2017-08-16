ed998ed2e34199e4cd9832e0d0e388b86ffc4aa1
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 11:32:24 -0500

    Cleaned up metadat.

--- recursif/__metadata__.py
+++ recursif/__metadata__.py
@@ -5,8 +5,6 @@ metadata = abjad.TypedOrderedDict([
     ('catalog_number', 'AWN-000-PR'),
     ('composer', 'Trevor Bača'),
     ('forces_tagline', 'for 64 pieces of percussion'),
-    ('paper_size', '11 x 17 in'),
-    ('price', '\\euro 88 \\/ \\$114'),
     ('title', 'Poème récursif'),
     ('year', 2003),
     ])
