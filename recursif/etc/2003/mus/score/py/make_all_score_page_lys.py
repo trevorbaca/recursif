3294d43454c8bf54a1aa896327aac2f937fcc835
Trevor Bača <trevor.baca@gmail.com>
2017-10-07 10:40:50 -0500

    Integrated f-strings.

--- recursif/etc/2003/mus/score/py/make_all_score_page_lys.py
+++ recursif/etc/2003/mus/score/py/make_all_score_page_lys.py
@@ -5,10 +5,10 @@ from make_poeme_2003_page_n import make_poeme_2003_page_n
 
 def make_all_score_page_lys(page_directory):
     for page_number in range(1, 1 + 1):
-        print 'Making Poeme recursif (2003) page {} ...'.format(page_number)
+        print(f'Making Poeme recursif (2003) page {page_number} ...')
         page_score = make_poeme_2003_page_n(page_number)
-        file_name = page_directory + 'poeme-2003-page-{}.ly'
-        file_name = file_name.format(str(page_number).zfill(2))
+        number = str(page_number).zfill(2)
+        file_name = page_directory + f'poeme-2003-page-{number}.ly'
         #persist(page_score).as_ly(file_name)
         lilypond_file = file(file_name, 'w')
         lilypond_file.write(page_score.format)
