6c861f0ae8936ee48fdc776b0d8f8521641b7398
Trevor Bača <trevor.baca@gmail.com>
2017-12-14 16:06:31 -0600

    Cleaned up initializers.

--- recursif/tools/__init__.py
+++ recursif/tools/__init__.py
@@ -1,7 +1,3 @@
-import abjad
-
-
-abjad.ImportManager.import_structured_package(
-    __path__[0],
-    globals(),
-    )
+from .ScoreTemplate import ScoreTemplate
+from .SegmentMaker import SegmentMaker
+from .integer_divisions import integer_divisions
