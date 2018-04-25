75d054c19bd2c3cd2c28a31033d896d43dd214fa
Trevor Bača <trevor.baca@gmail.com>
2018-04-25 16:14:17 -0500

    Cleaning up score.

--- recursif/tools/__init__.py
+++ recursif/tools/__init__.py
@@ -1,5 +1,4 @@
 from .ScoreTemplate import ScoreTemplate
-from .SegmentMaker import SegmentMaker
 from .integer_divisions import integer_divisions
 from .margin_markup import margin_markup
 from .rhythm import rhythm
