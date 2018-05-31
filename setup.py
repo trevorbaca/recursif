41b8bcfea126ab86bfb411b6ba688b39f7f1da74
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 14:16:17 -0500

    Cleaned up Travis configfile.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
         author_email='trevor.baca@gmail.com',
         install_requires=install_requires,
         keywords=keywords,
-        name='Poème récursif (2003)',
+        name='recursif',
         packages=['recursif'],
         platforms='Any',
         url='https://github.com/trevorbaca/recursif',
