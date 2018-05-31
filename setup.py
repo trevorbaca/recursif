d658677f789119dd141b0583781599d01ce4c76f
Trevor Bača <trevor.baca@gmail.com>
2018-05-31 14:10:51 -0500

    Cleaned up Travis configfile.

--- setup.py
+++ setup.py
@@ -2,14 +2,29 @@
 import setuptools
 
 
+install_requires = [
+    'abjad',
+    'mypy',
+    'roman',
+    ]
+
+keywords = [
+    'abjad',
+    'music composition',
+    'music notation',
+    'formalized score control',
+    'lilypond',
+    ]
+keywords = ', '.join(keywords)
+
 if __name__ == '__main__':
     setuptools.setup(
         author='Trevor Bača',
         author_email='trevor.baca@gmail.com',
-        install_requires=('abjad',),
-        name='recursif',
-        packages=('recursif',),
+        install_requires=install_requires,
+        keywords=keywords,
+        name='Poème récursif (2003)',
+        packages=['recursif'],
+        platforms='Any',
         url='https://github.com/trevorbaca/recursif',
-        version='0.1',
-        zip_safe=False,
         )
