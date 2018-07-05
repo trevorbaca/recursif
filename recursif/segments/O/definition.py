4f80c6f44ac816d26ca94b5ab25b2c4f59c546d8
Trevor Bača <trevor.baca@gmail.com>
2018-07-05 16:32:18 -0500

    Cleaned up metronome marks.

--- recursif/segments/O/definition.py
+++ recursif/segments/O/definition.py
@@ -10,7 +10,6 @@ import roman
 ###############################################################################
 
 maker = baca.SegmentMaker(
-    do_not_attach_metronome_mark_spanner=True,
     final_markup=(['Austin, TX.'], ['August 2003.']),
     final_markup_extra_offset=(10, 0),
     last_segment=True,
