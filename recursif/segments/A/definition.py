8d79d4167d4c5e053fc0e9ec3d510487123d34e4
Trevor Bača <trevor.baca@gmail.com>
2020-07-08 16:40:10 -0400

    Cleaned up path handling.

--- recursif/segments/A/definition.py
+++ recursif/segments/A/definition.py
@@ -1,5 +1,3 @@
-import os
-
 import roman
 
 import abjad
@@ -13,7 +11,7 @@ import recursif
 maker = baca.SegmentMaker(
     check_all_are_pitched=True,
     first_segment=True,
-    segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
+    segment_directory=abjad.Path(__file__).resolve().parent,
     time_signatures=16 * [(2, 4)],
     validate_measure_count=16,
 )
