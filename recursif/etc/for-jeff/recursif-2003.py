4597dd1ee1728855c1b6e7041f7964105951366b
Trevor Bača <trevor.baca@gmail.com>
2020-08-13 16:02:18 -0400

    blacked code.

--- recursif/etc/for-jeff/recursif-2003.py
+++ recursif/etc/for-jeff/recursif-2003.py
@@ -55,7 +55,7 @@ class SegmentMaker(baca.SegmentMaker):
         self._configure_lilypond_file()
         score_block = self.lilypond_file["score"]
         score = score_block["Score"]
-        if not abjad.wf.wellformed(score)
+        if not abjad.wf.wellformed(score):
             string = abjad.wf.tabulate_wellformedness(score)
             raise Exception(string)
         return self.lilypond_file
