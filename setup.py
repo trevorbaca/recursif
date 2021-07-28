a0c53217d52fae2190cdda84061c8615964cd2ef
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:31:48 -0400

    Cleaned up setup.py file.

--- setup.py
+++ setup.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python
 import setuptools
 
-install_requires = [
-    "abjad",
-    "black",
-    "flake8",
-    "isort",
-    "mypy",
-    "roman",
-]
-
-keywords = [
-    "abjad",
-    "lilypond",
-    "music composition",
-    "music notation",
-]
-
 if __name__ == "__main__":
     setuptools.setup(
         author="Trevor Bača",
         author_email="trevor.baca@gmail.com",
-        install_requires=install_requires,
-        keywords=", ".join(keywords),
+        install_requires=[
+            "abjad",
+            "black",
+            "flake8",
+            "isort",
+            "pytest",
+            "roman",
+        ],
+        keywords="abjad, lilypond, music composition, music notation",
         name="recursif",
         packages=["recursif"],
         platforms="Any",
