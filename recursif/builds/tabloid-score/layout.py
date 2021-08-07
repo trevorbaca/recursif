7fa42ef55d816a52e91e8957a088d4c02bec0315
Trevor Bača <trevor.baca@gmail.com>
2021-08-07 12:07:46 -0400

    Cleaned up layout files.

--- recursif/builds/tabloid-score/layout.py
+++ recursif/builds/tabloid-score/layout.py
@@ -1,12 +1,12 @@
 import baca
 
-after_global = 4
+below_global = 4
 between_staves = 7
 
 pages = []
 first_page = baca.page(
     baca.system(
-        after_global,
+        below_global,
         6,
         measure=1,
         y_offset=50,
@@ -18,12 +18,7 @@ pages.append(first_page)
 for page_number in range(2, 16 + 1):
     measure = (page_number - 1) * 16 + 1
     page = baca.page(
-        baca.system(
-            after_global,
-            between_staves,
-            measure=measure,
-            y_offset=0,
-        ),
+        baca.system(measure=measure, y_offset=0, (below_global, between_staves)),
         number=page_number,
     )
     pages.append(page)
