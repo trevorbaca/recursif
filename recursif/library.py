de7f9678ad892202d62429e9233d8ee873f063ef
Trevor Bača <trevor.baca@gmail.com>
2021-09-15 14:45:28 +0200

    Cleaned up empty score construction.

--- recursif/library.py
+++ recursif/library.py
@@ -91,7 +91,7 @@ class ScoreTemplate(baca.ScoreTemplate):
         site = "recursif.ScoreTemplate.__call__()"
         tag = abjad.Tag(site)
         # GLOBAL CONTEXT
-        global_context = self._make_global_context()
+        global_context = baca.templates.make_global_context()
 
         super(ScoreTemplate, self).__init__()
         staves = []
@@ -115,9 +115,9 @@ class ScoreTemplate(baca.ScoreTemplate):
 
         # SCORE
         score = abjad.Score([global_context, music_context], name="Score", tag=tag)
-        self._assert_lilypond_identifiers(score)
-        self._assert_unique_context_names(score)
-        self._assert_matching_custom_context_names(score)
+        baca.templates.assert_lilypond_identifiers(score)
+        baca.templates.assert_unique_context_names(score)
+        baca.templates.assert_matching_custom_context_names(score)
         return score
 
     ### PUBLIC PROPERTIES ###
