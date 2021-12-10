de063be390337f518e758220328a0284821560f8
Trevor Bača <trevor.baca@gmail.com>
2021-12-10 14:22:59 +0100

    Cleaned up music.py files.

--- recursif/segments/09/music.py
+++ recursif/segments/09/music.py
@@ -1,31 +1,31 @@
 import baca
 
-from recursif import library as recursif
+from recursif import library
 
 #########################################################################################
 ########################################### 09 ##########################################
 #########################################################################################
 
-score = recursif.make_empty_score()
+score = library.make_empty_score()
 voice_names = baca.accumulator.get_voice_names(score)
 
 commands = baca.CommandAccumulator(
     **baca.segment_accumulation_defaults(),
-    instruments=recursif.instruments,
-    margin_markups=recursif.margin_markups,
-    metronome_marks=recursif.metronome_marks,
+    instruments=library.instruments,
+    margin_markups=library.margin_markups,
+    metronome_marks=library.metronome_marks,
     time_signatures=16 * [(2, 4)],
     voice_names=voice_names,
 )
 
-recursif.assign_parts(commands)
+library.assign_parts(commands)
 
 for voice_number in range(1, 64 + 1):
     voice_name = f"Percussion.Voice.{voice_number}"
     commands(
         voice_name,
         baca.staff_position(0),
-        recursif.rhythm(voice_number, 9),
+        library.rhythm(voice_number, 9),
     )
 
 if __name__ == "__main__":
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         always_make_global_rests=True,
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
-        part_manifest=recursif.part_manifest,
+        part_manifest=library.part_manifest,
     )
     lilypond_file = baca.make_lilypond_file(
         score,
--- recursif/segments/16/music.py
+++ recursif/segments/16/music.py
@@ -1,20 +1,20 @@
 import abjad
 import baca
 
-from recursif import library as recursif
+from recursif import library
 
 #########################################################################################
 ########################################### 16 ##########################################
 #########################################################################################
 
-score = recursif.make_empty_score()
+score = library.make_empty_score()
 voice_names = baca.accumulator.get_voice_names(score)
 
 commands = baca.CommandAccumulator(
     **baca.segment_accumulation_defaults(),
-    instruments=recursif.instruments,
-    margin_markups=recursif.margin_markups,
-    metronome_marks=recursif.metronome_marks,
+    instruments=library.instruments,
+    margin_markups=library.margin_markups,
+    metronome_marks=library.metronome_marks,
     time_signatures=16 * [(2, 4)],
     voice_names=voice_names,
 )
@@ -24,14 +24,14 @@ commands(
     baca.bar_line("|.", baca.selectors.skip(-1)),
 )
 
-recursif.assign_parts(commands)
+library.assign_parts(commands)
 
 for voice_number in range(1, 64 + 1):
     voice_name = f"Percussion.Voice.{voice_number}"
     commands(
         voice_name,
         baca.staff_position(0),
-        recursif.rhythm(voice_number, 16),
+        library.rhythm(voice_number, 16),
     )
 
 commands(
@@ -54,7 +54,7 @@ if __name__ == "__main__":
         error_on_not_yet_pitched=True,
         final_segment=True,
         global_rests_in_topmost_staff=True,
-        part_manifest=recursif.part_manifest,
+        part_manifest=library.part_manifest,
     )
     lilypond_file = baca.make_lilypond_file(
         score,
