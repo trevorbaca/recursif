bf7815a4198a5963ad3474421755396602d08fcf
Trevor Bača <trevor.baca@gmail.com>
2023-01-13 17:05:44 -0500

    Bumped Python to 3.11.

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
         name="recursif",
         packages=["recursif"],
         platforms="Any",
-        python_requires=">=3.10",
+        python_requires=">=3.11",
         url="https://github.com/trevorbaca/recursif",
     )
