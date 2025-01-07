e7b370e344e27f6f5efd7251ac35c84854d2081c
Trevor Bača <trevor.baca@gmail.com>
2025-01-07 15:44:04 -0500

    Clean up __init__.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         name="recursif",
         packages=["recursif"],
         platforms="Any",
-        python_requires=">=3.10",
+        python_requires=">=3.12",
         url="https://github.com/trevorbaca/recursif",
         version="3.19",
     )
