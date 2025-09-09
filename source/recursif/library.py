b67c234442443966981a67e6a48dab56001ff7aa
Trevor Bača <trevor.baca@gmail.com>
2025-09-09 10:47:20 -0400

    Change abjad.ValueDuration to abjad.Duration

--- source/recursif/library.py
+++ source/recursif/library.py
@@ -1121,7 +1121,7 @@ instruments = {
 
 metronome_marks = {
     "38-42": abjad.MetronomeMark(
-        abjad.ValueDuration(1, 2), 40, textual_indication='"38-42"'
+        abjad.Duration(1, 2), 40, textual_indication='"38-42"'
     ),
 }
 
